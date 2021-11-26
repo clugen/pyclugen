605d40e01edd3d65ec906e2f0fb26227e9c5d0bc
Nuno Fachada <faken@fakenmc.com>
2021-11-26 00:40:57 +0000

    Add type hints to points_on_line()

--- src/clugen/core.py
+++ src/clugen/core.py
@@ -3,7 +3,11 @@
 """
 
 import numpy as np
+import numpy.typing as npt
 
-def points_on_line(center, direction, dist_center):
+def points_on_line(
+    center: npt.NDArray,
+    direction: npt.NDArray,
+    dist_center: npt.NDArray) -> npt.NDArray:
 
     return center.T + dist_center @ direction.T
