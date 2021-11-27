2e6461e71bffb6e3ae59aaad40b225cfe5aeb537
Nuno Fachada <faken@fakenmc.com>
2021-11-27 23:01:38 +0000

    Style improvements

--- clugen/core.py
+++ clugen/core.py
@@ -5,9 +5,9 @@
 import numpy as np
 import numpy.typing as npt
 
+
 def points_on_line(
-    center: npt.NDArray,
-    direction: npt.NDArray,
-    dist_center: npt.NDArray) -> npt.NDArray:
+    center: npt.NDArray, direction: npt.NDArray, dist_center: npt.NDArray
+) -> npt.NDArray:
 
     return center.T + dist_center @ direction.T
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import clugen as cg
 
+
 def test_points_on_line():
     assert True
