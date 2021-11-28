b83fe3c0a348c04119ecdeb9fe7e89cc6bb4307a
Nuno Fachada <faken@fakenmc.com>
2021-11-28 19:22:32 +0000

    Add doctest to points_on_line()

--- clugen/core.py
+++ clugen/core.py
@@ -11,6 +11,20 @@ def points_on_line(
     Determine coordinates of points on a line with `center` and `direction`,
     based on the distances from the center given in `dist_center`.
 
+    Example:
+
+    >>> import clugen as cg
+    >>> import numpy as np
+    >>> center = np.array([[5.0, 5.0]]).T
+    >>> dir = np.array([[1.0, 0.0]]).T
+    >>> dc = np.array([np.linspace(-4, 4, 5)]).T
+    >>> cg.points_on_line(center, dir, dc)
+    array([[1., 5.],
+           [3., 5.],
+           [5., 5.],
+           [7., 5.],
+           [9., 5.]])
+
     Args:
       center: Todo.
       direction: Todo.
