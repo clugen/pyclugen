68b9c07506fed6c77e40cfbef67c8de2786f8d2d
Nuno Fachada <faken@fakenmc.com>
2021-11-28 23:36:42 +0000

    Add license header to code files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
+# Copyright (c) 2020, 2021 Nuno Fachada and contributors
+# Distributed under the MIT License (See accompanying file LICENSE.txt or copy
+# at http://opensource.org/licenses/MIT)
+
 """Tests for the pyclugen project."""
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2020, 2021 Nuno Fachada and contributors
+# Distributed under the MIT License (See accompanying file LICENSE.txt or copy
+# at http://opensource.org/licenses/MIT)
+
 """Tests for the core module."""
 
 
