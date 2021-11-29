59b06338036bb4d5dcb62877e164a56bbcfabe1e
Nuno Fachada <faken@fakenmc.com>
2021-11-29 15:49:22 +0000

    Add tests for points_on_line()

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,56 @@
 
 """Fixtures to be used by test functions."""
 
+import numpy as np
 import pytest
+
+
+@pytest.fixture(params=[0, 123, 9999, 9876543])
+def seed(request):
+    """Provides PRNG seeds."""
+    return request.param
+
+
+@pytest.fixture()
+def prng(seed):
+    """Provides random number generators."""
+    return np.random.default_rng(seed)
 
 
 @pytest.fixture(params=[1, 2, 3, 4, 30])
 def ndims(request):
-    """Provides the number of dimensions to test with."""
+    """Provides a number of dimensions."""
     return request.param
+
+
+@pytest.fixture(params=[1, 10, 500, 10000])
+def num_points(request):
+    """Provides a number of points."""
+    return request.param
+
+
+@pytest.fixture(params=[0, 10])
+def llength_mu(request):
+    """Provides a line length average."""
+    return request.param
+
+
+@pytest.fixture()
+def vector(prng):
+    """Provides random vectors."""
+
+    def _vector(numel):
+        return prng.random((numel, 1))
+
+    return _vector
+
+
+@pytest.fixture()
+def uvector(prng):
+    """Provides random unit vectors."""
+
+    def _uvector(numel):
+        v = prng.random((numel, 1))
+        return v / np.linalg.norm(v)
+
+    return _uvector
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,15 +4,55 @@
 
 """Tests for the core module."""
 
-import numpy as np
+import pytest
+from numpy import vdot
+from numpy.linalg import norm
+from numpy.testing import assert_allclose
 
 import clugen as cg
 
 
-def test_points_on_line(ndims):
-    """Test points_on_line()."""
-    ctr = np.ones((ndims, 1))
-    direc = np.ones((ndims, 1)) * 2
-    dc = np.array([[-1.5, 2, -1.6, 10, 0, 1.1]]).T
-    pol = cg.points_on_line(ctr, direc, dc)
-    assert pol.shape == (dc.size, ndims)
+def test_points_on_line(ndims, num_points, prng, llength_mu, uvector, vector):
+    """Test the points_on_line function."""
+    # Number of directions to test
+    ndirs = 3
+
+    # Number of line centers to test
+    ncts = 3
+
+    # Avoid too many points, otherwise testing will be very slow
+    if num_points >= 1000:
+        return
+
+    # Create some random distances from center
+    dist2ctr = llength_mu * prng.random((num_points, 1)) - llength_mu / 2
+
+    # Test for different directions
+    for _i in range(ndirs):
+
+        # Get a direction
+        direc = uvector(ndims)
+
+        # Test for different number of line centers
+        for _j in range(ncts):
+
+            # Get a line center
+            ctr = vector(ndims)
+
+            # Invoke the points_on_line function
+            with pytest.warns(None) as wrec:
+                pts = cg.points_on_line(ctr, direc, dist2ctr)
+
+            # Check that the points_on_line function runs without warnings
+            assert len(wrec) == 0
+
+            # Check that the dimensions agree
+            assert pts.shape == (num_points, ndims)
+
+            # Check that distance of points to the line is approximately zero
+            for pt in pts:
+                # Get distance from current point to line
+                pt = pt.reshape((ndims, 1))
+                d = norm((pt - ctr) - vdot((pt - ctr), direc) * direc)
+                # Check that it is approximately zero
+                assert_allclose(d, 0, atol=1e-14)
