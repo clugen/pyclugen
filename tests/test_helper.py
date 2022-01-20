27c21baf2e55e6a9986b48df2357834ef29d0127
Nuno Fachada <faken@fakenmc.com>
2022-01-20 20:30:38 +0000

    Add tests for fix_num_points()

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -7,11 +7,11 @@
 import numpy as np
 from numpy.testing import assert_equal
 
-from clugen.helper import fix_empty
+from clugen.helper import fix_empty, fix_num_points
 
 
 def test_fix_empty():
-    """Test the points_on_line() function."""
+    """Test the fix_empty() function."""
     # No empty clusters
     clusts = np.array([11, 21, 10])
     clusts_copy = np.copy(clusts)
@@ -64,3 +64,56 @@ def test_fix_empty():
     clusts_fixed = fix_empty(clusts, True)
     assert clusts is clusts_fixed
     assert_equal(clusts_copy, clusts_fixed)
+
+
+def test_fix_num_points():
+    """Test the fix_num_points() function."""
+    # No change
+    clusts = np.array([10, 100, 42, 0, 12])
+    clusts_copy = np.copy(clusts)
+    clusts_fixed = fix_num_points(clusts, np.sum(clusts))
+    assert clusts is clusts_fixed
+    assert_equal(clusts_copy, clusts_fixed)
+
+    # # Fix due to too many points
+    clusts = np.array([55, 12])
+    clusts_copy = np.copy(clusts)
+    num_pts = sum(clusts) - 14
+    clusts_fixed = fix_num_points(clusts, num_pts)
+    assert clusts is clusts_fixed
+    assert np.any(clusts_copy != clusts_fixed)
+    assert np.sum(clusts_fixed) == num_pts
+
+    # Fix due to too few points
+    clusts = np.array([0, 1, 0, 0])
+    clusts_copy = np.copy(clusts)
+    num_pts = 15
+    clusts_fixed = fix_num_points(clusts, num_pts)
+    assert clusts is clusts_fixed
+    assert np.any(clusts_copy != clusts_fixed)
+    assert np.sum(clusts_fixed) == num_pts
+
+    # 1D - No change
+    clusts = np.array([10])
+    clusts_copy = np.copy(clusts)
+    clusts_fixed = fix_num_points(clusts, sum(clusts))
+    assert clusts is clusts_fixed
+    assert_equal(clusts_copy, clusts_fixed)
+
+    # 1D - Fix due to too many points
+    clusts = np.array([241])
+    clusts_copy = np.copy(clusts)
+    num_pts = sum(clusts) - 20
+    clusts_fixed = fix_num_points(clusts, num_pts)
+    assert clusts is clusts_fixed
+    assert np.any(clusts_copy != clusts_fixed)
+    assert np.sum(clusts_fixed) == num_pts
+
+    # 1D - Fix due to too few points
+    clusts = np.array([0])
+    clusts_copy = np.copy(clusts)
+    num_pts = 8
+    clusts_fixed = fix_num_points(clusts, num_pts)
+    assert clusts is clusts_fixed
+    assert np.any(clusts_copy != clusts_fixed)
+    assert np.sum(clusts_fixed) == num_pts
