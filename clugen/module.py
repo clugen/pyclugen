eebda9d7940d08bf66f5db55d28ebc87a51ed99c
Nuno Fachada <faken@fakenmc.com>
2022-01-22 01:31:18 +0000

    Implement clucenters()

--- clugen/module.py
+++ clugen/module.py
@@ -4,15 +4,30 @@
 
 """Algorithm module functions."""
 
+from numpy import diag
+from numpy.random import Generator
+from numpy.typing import NDArray
+
+from .shared import _default_rng
+
 
 def angle_deltas():
     """Placeholder."""
     pass
 
 
-def clucenters():
+def clucenters(
+    num_clusters: int,
+    clu_sep: NDArray,
+    clu_offset: NDArray,
+    rng: Generator = _default_rng,
+) -> NDArray:
     """Placeholder."""
-    pass
+    # Obtain a num_clusters x num_dims matrix of uniformly distributed values
+    # between -0.5 and 0.5 representing the relative cluster centers
+    ctr_rel = rng.random((num_clusters, clu_sep.size)) - 0.5
+
+    return num_clusters * (ctr_rel @ diag(clu_sep)) + clu_offset
 
 
 def clupoints_n_1():
