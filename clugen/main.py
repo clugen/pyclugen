a3fbd3e391f6d48a3db7adf55e4c894871028200
Nuno Fachada <faken@fakenmc.com>
2022-01-24 17:03:57 +0000

    Add main clugen() function prototype

--- clugen/main.py
+++ clugen/main.py
@@ -4,7 +4,35 @@
 
 """Main function."""
 
+from typing import Callable, NamedTuple, Optional, Union
 
-def clugen():
+from numpy.random import Generator
+from numpy.typing import NDArray
+
+from .module import angle_deltas, clucenters, clusizes, llengths
+from .shared import _default_rng
+
+
+def clugen(
+    num_dims: int,
+    num_clusters: int,
+    num_points: int,
+    direction: NDArray,
+    angle_disp: float,
+    llength: float,
+    llength_disp: float,
+    lateral_disp: float,
+    allow_empty: bool = False,
+    cluster_offset: Optional[NDArray] = None,
+    proj_dist_fn: Union[str, Callable[[float, int], NDArray]] = "norm",
+    point_dist_fn: Union[
+        str, Callable[[NDArray, float, float, NDArray, NDArray, Generator], NDArray]
+    ] = "n-1",
+    clusizes_fn: Callable[[int, int, bool, Generator], NDArray] = clusizes,
+    clucenters_fn: Callable[[int, NDArray, NDArray, Generator], NDArray] = clucenters,
+    llengths_fn: Callable[[int, float, float, Generator], NDArray] = llengths,
+    angle_deltas_fn: Callable[[int, float, Generator], NDArray] = angle_deltas,
+    rng: Generator = _default_rng,
+) -> NamedTuple:
     """Placeholder."""
     pass
