ce0a198510c17ffe1eacf13b1941472684b10b70
Nuno Fachada <faken@fakenmc.com>
2022-01-24 19:10:58 +0000

    clugen(): more arg checks + accepts ArrayLike instead of NDArray

--- clugen/main.py
+++ clugen/main.py
@@ -6,10 +6,10 @@
 
 from typing import Callable, NamedTuple, Optional, Union
 
-from numpy import array, isclose
+from numpy import array, asarray, isclose, zeros
 from numpy.linalg import norm
 from numpy.random import Generator
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike, NDArray
 
 from .module import angle_deltas, clucenters, clusizes, llengths
 from .shared import _default_rng
@@ -47,13 +47,14 @@ def clugen(
     num_dims: int,
     num_clusters: int,
     num_points: int,
-    direction: NDArray,
+    direction: ArrayLike,
     angle_disp: float,
+    cluster_sep: ArrayLike,
     llength: float,
     llength_disp: float,
     lateral_disp: float,
     allow_empty: bool = False,
-    cluster_offset: Optional[NDArray] = None,
+    cluster_offset: Optional[ArrayLike] = None,
     proj_dist_fn: Union[str, Callable[[float, int], NDArray]] = "norm",
     point_dist_fn: Union[
         str, Callable[[NDArray, float, float, NDArray, NDArray, Generator], NDArray]
@@ -80,6 +81,42 @@ def clugen(
     # Check that direction vector has magnitude > 0
     if isclose(norm(direction), 0):
         raise ValueError("`direction` must have magnitude > 0")
+
+    # Check that direction has num_dims dimensions
+    direction = asarray(direction)
+    if direction.size != num_dims:
+        raise ValueError(
+            "Length of `direction` must be equal to `num_dims` "
+            + f"({direction.size} != {num_dims})"
+        )
+
+    # If allow_empty is false, make sure there are enough points to distribute
+    # by the clusters
+    if (not allow_empty) and num_points < num_clusters:
+        raise ValueError(
+            "A total of $num_points points is not enough for "
+            + f"{num_clusters} non-empty clusters"
+        )
+
+    # Check that cluster_sep has num_dims dimensions
+    cluster_sep = asarray(cluster_sep)
+    if cluster_sep.size != num_dims:
+        raise ValueError(
+            "Length of `cluster_sep` must be equal to `num_dims` "
+            + f"({cluster_sep.size} != {num_dims})"
+        )
+
+    # If given, cluster_offset must have the correct number of dimensions,
+    # if not given then it will be a num_dims x 1 vector of zeros
+    if cluster_offset is None:
+        cluster_offset = zeros(num_dims)
+    else:
+        cluster_offset = asarray(cluster_offset)
+        if cluster_offset.size != num_dims:
+            raise ValueError(
+                "Length of `cluster_offset` must be equal to `num_dims` "
+                + f"({cluster_offset.size} != {num_dims}"
+            )
 
     return Clusters(
         array([]),
