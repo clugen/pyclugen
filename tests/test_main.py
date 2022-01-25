3a1a575277d4e18d1469969c3566bde595f7298e
Nuno Fachada <faken@fakenmc.com>
2022-01-25 15:38:10 +0000

    Add optional param tests for clugen()

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,11 +5,12 @@
 """Tests for the main clugen() function."""
 
 import pytest
-from numpy import abs, all, arange, pi, sum, unique
+from numpy import abs, all, arange, diag, linspace, ones, pi, sum, unique, zeros
 from numpy.random import PCG64, Generator
 from numpy.testing import assert_allclose
 
 from clugen.main import clugen
+from clugen.module import angle_deltas, clucenters, clusizes, llengths
 
 from .helpers import angle_btw
 
@@ -44,7 +45,7 @@ def lat_std(request):
     return request.param
 
 
-@pytest.fixture(params=[0, pi / 32, pi / 2, pi, 2 * pi])
+@pytest.fixture(params=[0, pi / 8, pi])
 def angle_std(request):
     """Provides an angle standard deviation."""
     return request.param
@@ -124,6 +125,139 @@ def test_clugen_mandatory(
     # Check that cluster directions have the correct angles with the main direction
     if ndims > 1:
         for i in range(num_clusters):
+            assert_allclose(
+                angle_btw(direc, result.cluster_directions[i, :]),
+                abs(result.cluster_angles[i]),
+                atol=1e-11,
+            )
+
+
+@pytest.fixture(params=["norm", "unif", lambda l, n: linspace(-l / 2, l / 2, n)])
+def ptdist_fn(request):
+    """Provides a point distribution function."""
+    return request.param
+
+
+@pytest.fixture(
+    params=["n-1", "n", lambda prj, ls, l, cd, cc, r: prj + ones(prj.shape)]
+)
+def ptoff_fn(request):
+    """Provides a point offset function."""
+    return request.param
+
+
+def csz_equi_size(nclu, tpts, ae, rng):
+    """Alternative cluster sizing function for testing purposes."""
+    cs = zeros(nclu, dtype=int)
+    for i in range(tpts):
+        cs[i % nclu] += 1
+    return cs
+
+
+@pytest.fixture(params=[clusizes, csz_equi_size])
+def csz_fn(request):
+    """Provides a cluster sizes function."""
+    return request.param
+
+
+@pytest.fixture(
+    params=[
+        clucenters,
+        lambda nc, cs, co, r: diag(arange(1, nc + 1)) @ ones((nc, cs.size)),
+    ]
+)
+def cctr_fn(request):
+    """Provides a cluster centers function."""
+    return request.param
+
+
+@pytest.fixture(params=[llengths, lambda nc, l, ls, r: 10 + 10 * r.random(nc)])
+def llen_fn(request):
+    """Provides a a line lengths function."""
+    return request.param
+
+
+@pytest.fixture(params=[angle_deltas, lambda nc, asd, r: zeros(nc)])
+def lang_fn(request):
+    """Provides a line angles function."""
+    return request.param
+
+
+def test_clugen_optional(
+    prng,
+    ndims,
+    vector,
+    clu_sep,
+    clu_offset,
+    allow_empty,
+    ptdist_fn,
+    ptoff_fn,
+    csz_fn,
+    cctr_fn,
+    llen_fn,
+    lang_fn,
+):
+    """Test the optional parameters of the clugen() function."""
+    # Valid arguments
+    nclu = 7
+    tpts = 500
+    astd = pi / 256
+    len_mu = 9
+    len_std = 1.2
+    lat_std = 2
+
+    # Get direction
+    direc = vector(ndims)
+
+    with pytest.warns(None) as wrec:
+        result = clugen(
+            ndims,
+            nclu,
+            tpts,
+            direc,
+            astd,
+            clu_sep,
+            len_mu,
+            len_std,
+            lat_std,
+            allow_empty=allow_empty,
+            cluster_offset=clu_offset,
+            proj_dist_fn=ptdist_fn,
+            point_dist_fn=ptoff_fn,
+            clusizes_fn=csz_fn,
+            clucenters_fn=cctr_fn,
+            llengths_fn=llen_fn,
+            angle_deltas_fn=lang_fn,
+            rng=prng,
+        )
+
+    # Check that the function runs without warnings
+    assert len(wrec) == 0
+
+    # Check dimensions of result variables
+    assert result.points.shape == (tpts, ndims)
+    assert result.point_clusters.shape == (tpts,)
+    assert result.point_projections.shape == (tpts, ndims)
+    assert result.cluster_sizes.shape == (nclu,)
+    assert result.cluster_centers.shape == (nclu, ndims)
+    assert result.cluster_directions.shape == (nclu, ndims)
+    assert result.cluster_angles.shape == (nclu,)
+    assert result.cluster_lengths.shape == (nclu,)
+
+    # Check point cluster indexes
+    if not allow_empty:
+        assert all(unique(result.point_clusters) == arange(nclu))
+    else:
+        assert all(result.point_clusters < nclu)
+
+    # Check total points
+    assert sum(result.cluster_sizes) == tpts
+    # This might not be the case if the specified clusize_fn does not obey
+    # the total number of points
+
+    # Check that cluster directions have the correct angles with the main direction
+    if ndims > 1:
+        for i in range(nclu):
             assert_allclose(
                 angle_btw(direc, result.cluster_directions[i, :]),
                 abs(result.cluster_angles[i]),
