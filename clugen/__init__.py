b66ad9993f9323d36b18846b0d45c4e470d4002b
Nuno Fachada <faken@fakenmc.com>
2022-04-19 18:14:16 +0100

    Docs: rewrite intro do API docs in __init__.py

--- clugen/__init__.py
+++ clugen/__init__.py
@@ -2,7 +2,17 @@
 # Distributed under the MIT License (See accompanying file LICENSE.txt or copy
 # at http://opensource.org/licenses/MIT)
 
-"""Multidimensional cluster generation in Python."""
+"""Various functions for multidimensional cluster generation in Python.
+
+Note that:
+
+1. [`clugen()`][clugen.main.clugen] is the main function of the **pyclugen**
+   package, and possibly the only function most users will need.
+2. Functions which accept `rng` as the last parameter are stochastic. Thus, in
+   order to obtain the same result on separate invocations of these functions,
+   pass them an an instance of same pseudo-random number
+   [`Generator`][numpy.random.Generator] initialized with the same seed.
+"""
 
 
 __all__ = [
