bb08f17949e46f58b0d07f2a0c12cfbee4d9cea9
Nuno Fachada <faken@fakenmc.com>
2022-07-01 17:27:49 +0100

    Docs: add first two sets of 2D examples

--- docs/docs/examples/plot_2_2d_examples.py
+++ docs/docs/examples/plot_2_2d_examples.py
@@ -1,39 +1,70 @@
 """# Examples in 2D
 
-These are the 2D examples.
-"""
-
-import matplotlib.pyplot as plt
-from numpy.random import PCG64, Generator
+This section contains several examples on how to generate 2D data with
+**pyclugen**. To run the examples we first need to import the
+[`clugen()`][clugen.main.clugen] function:"""
 
 from clugen import clugen
 
+#%%
+# To make the examples exactly reproducible we'll import a random number
+# generator from NumPy and pass it as a parameter to
+# [`clugen()`][clugen.main.clugen]. We'll also create a small helper function
+# for providing us a brand new seeded generator:
+
+from numpy import pi
+from numpy.random import PCG64, Generator, default_rng
 
 def rng(seed):
     return Generator(PCG64(seed))
 
+#%%
+# To plot these examples we use the [`plot_examples_2d`](plot_functions.md#plot_examples_2d)
+# function:
+
+from plot_functions import plot_examples_2d
+
 
 #%%
+# ## Manipulating the direction of cluster-supporting lines
+#
+# ### Using the `direction` parameter
 
 seed = 123
 
 #%%
 
-e01 = clugen(2, 4, 200, [1, 0], 0, [10, 10], 10, 1.5, 0.5, rng=rng(seed))
+e01 = clugen(2, 4, 2000, [1, 0], 0, [10, 10], 10, 1.5, 0.5, rng=rng(seed))
 e02 = clugen(2, 4, 200, [1, 1], 0, [10, 10], 10, 1.5, 0.5, rng=rng(seed))
 e03 = clugen(2, 4, 200, [0, 1], 0, [10, 10], 10, 1.5, 0.5, rng=rng(seed))
 
 #%%
 
-plt.scatter(e01.points[:, 0], e01.points[:, 1], c=e01.clusters)
-plt.show()
+plot_examples_2d(
+    e01, "e01: direction = [1, 0]",
+    e02, "e02: direction = [1, 1]",
+    e03, "e03: direction = [0, 1]")
+
+#%%
+# ### Changing the `angle_disp` parameter and using a custom `angle_deltas_fn` function
+
+seed = 321
 
 #%%
 
-plt.scatter(e02.points[:, 0], e02.points[:, 1], c=e02.clusters)
-plt.show()
+# Custom angle_deltas function: arbitrarily rotate some clusters by 90 degrees
+def angdel_90_fn(nclu, astd, rng):
+    return rng.choice([0, pi / 2], size=nclu)
 
 #%%
 
-plt.scatter(e03.points[:, 0], e03.points[:, 1], c=e03.clusters)
-plt.show()
+e04 = clugen(2, 6, 500, [1, 0], 0, [10, 10], 10, 1.5, 0.5, rng=rng(seed))
+e05 = clugen(2, 6, 500, [1, 0], pi / 8, [10, 10], 10, 1.5, 0.5, rng=rng(seed))
+e06 = clugen(2, 6, 500, [1, 0], 0, [10, 10], 10, 1.5, 0.5, rng=rng(seed), angle_deltas_fn=angdel_90_fn)
+
+#%%
+
+plot_examples_2d(
+    e04, "e04: angle_disp = 0",
+    e05, "e05: angle_disp = π/8",
+    e06, "e06: custom angle_deltas function")
---
+++ docs/docs/examples/plot_functions.py
@@ -0,0 +1,95 @@
+"""# Plot functions
+
+Several auxiliary functions for plotting the examples in this documentation.
+"""
+
+#%%
+# ## Import the required libraries
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from clugen import Clusters
+
+
+#%%
+# ## Set seaborn's theme
+
+sns.set_theme(style="darkgrid")
+
+
+#%%
+# ## clusters2df
+
+def clusters2df(*exs: Clusters) -> pd.DataFrame:
+    """Convert a sequence of clusters to a Pandas dataframe."""
+
+    dfs = []
+    iex = 1
+
+    for ex in exs:
+        df = pd.DataFrame(data=ex.points, columns=["x", "y"])
+        df["cluster"] = ex.clusters.tolist()
+        df["example"] = [iex] * ex.clusters.size
+        dfs.append(df)
+        iex += 1
+
+    return pd.concat(dfs, ignore_index=True)
+
+
+#%%
+# ## get_plot_lims
+
+def get_plot_lims(df: pd.DataFrame, pmargin: float = 0.1):
+    """Determine the plot limits for the cluster data given in `df`."""
+
+    # Get maximum and minimum points in each dimension
+    xmaxs = df[["x", "y"]].max()
+    xmins = df[["x", "y"]].min()
+
+    # Determine plot centers in each dimension
+    xcenters = (xmaxs + xmins) / 2
+
+    # Determine plots span for all dimensions
+    sidespan = (1 + pmargin) * np.max(np.abs(xmaxs - xmins)) / 2
+
+    # Determine final plots limits
+    xmaxs = xcenters + sidespan
+    xmins = xcenters - sidespan
+
+    return xmaxs, xmins
+
+
+#%%
+# ## plot_examples_2d
+
+def plot_examples_2d(*ets, pmargin: float = 0.1, ncols: int = 3):
+    """Plot the 2D examples given in the ets parameter."""
+
+    # Get examples
+    ex = ets[0::2]
+    # Get titles
+    et = ets[1::2]
+
+    df = clusters2df(*ex)
+
+    # Get limits in each dimension
+    xmaxs, xmins = get_plot_lims(df, pmargin=pmargin)
+
+    # Use seaborn to create the plots
+    g = sns.FacetGrid(
+        df,
+        col="example",
+        hue="cluster",
+        xlim=(xmins[0], xmaxs[0]),
+        ylim=(xmins[1], xmaxs[1]),
+        aspect=1,
+        col_wrap=ncols,
+    )
+
+    g.map(sns.scatterplot, "x", "y")
+
+    # Set the plot titles
+    for ax, t in zip(g.axes, et):
+        ax.set_title(t)
